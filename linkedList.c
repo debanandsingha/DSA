e49e360f259183e2276405cebbe2f574c8ad1acb
Debanand Singha <debanand1101@gmail.com>
2023-11-11 01:31:26 +0530

    linkedList: completed (singly linked list)

--- linkedList.c
+++ linkedList.c
@@ -1,6 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+void createLinkedList();
+void displayLinkedList();
+int getLength();
+
+void insertBeg();
+void insertEnd();
+void insertPos();
+
+void deleteBeg();
+void deleteEnd();
+void deletePos();
+
 struct node
 {
     int data;
@@ -8,6 +20,24 @@ struct node
 };
 
 struct node *head=NULL;
+
+
+void main(){
+
+    createLinkedList();
+    insertBeg();
+    insertEnd();
+    insertPos();
+    displayLinkedList();
+
+    deleteBeg();
+    deleteEnd();
+    deletePos();
+
+    displayLinkedList();
+
+}
+
 
 void createLinkedList(){
     struct node *new, *temp;
@@ -23,6 +53,7 @@ void createLinkedList(){
 
         if(head==NULL){
             head=temp=new;
+            head->next=NULL;
         }else{
             temp->next=new;
             temp=new;
@@ -80,12 +111,93 @@ void insertEnd(){
     
     new->next=NULL; // setting the value of last node to NULL, undefined by default
 
-    printf("\nNode Inserted!");
+    printf("\nNode Inserted!\n");
     
 }
 
 void insertPos(){
 
+    int pos;
+    printf("\nEnter position after which node is to be inserted: ");
+    scanf("%d", &pos);
+
+    //check if the given position is valid
+    if(pos>getLength()){
+        printf("Invalid Position!");
+    }else{
+        struct node *new, *temp;
+
+        //Creating node
+        new=(struct node *)malloc(sizeof(struct node));
+        printf("Enter data: ");
+        scanf("%d", &new->data);
+
+        //Traverse to the postion after which the new node is to be inserted
+        temp=head;
+        int i=1;
+        while(i<pos){
+            temp=temp->next;
+            i++;
+        }
+
+        new->next = temp->next; //setting the pointer of the new node to the node which will be after it
+        temp->next = new; // pointing the node to the new node
+
+        printf("Node inserted! ");
+    }
+}
+
+void deleteBeg(){
+    struct node *temp;
+
+    temp=head;
+    head=head->next;
+    free(temp);
+}
+
+void deleteEnd(){
+    struct node *temp, *prev;
+
+    temp=head;
+    while(temp->next != NULL){
+        prev=temp;  //store temp pointer in prev pointer
+        temp=temp->next; //update temp which is point to next node
+    }
+
+    if(temp==head){
+        head=NULL;
+    }else{
+        prev->next=NULL;
+    }
+    
+    free(temp);
+}
+
+void deletePos(){
+    struct node *temp, *prev;
+
+    int pos;
+    printf("\n\nEnter position of node to delete: ");
+    scanf("%d",&pos);
+
+    temp=head;
+
+    if(pos>getLength()){
+        printf("Invalid Position!");
+    }else if(pos==1){
+        deleteBeg();
+    }else{
+        int i=1;
+        while(i<pos){
+            prev=temp;
+            temp=temp->next;
+
+            i++;
+        }
+
+        prev->next=temp->next;
+        free(temp);
+    }
 }
 
 int getLength(){
@@ -99,16 +211,4 @@ int getLength(){
     }
 
     return length;
-}
-
-void main(){
-
-    createLinkedList();     //creating linked list
-
-    insertBeg();    //inserting node in the beginning
-
-    insertEnd();    //inserting node at the end
-
-    displayLinkedList();    //displaying the linked list
-
 }
