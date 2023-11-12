1a1b72e3acde223c6c5be41dd1c82a2badce1c78
Debanand Singha <debanand1101@gmail.com>
2023-11-12 20:41:15 +0530

    doublyLinkedList: INSERT(Begg, End, Pos)

--- doublyLinkedList.c
+++ doublyLinkedList.c
@@ -5,6 +5,12 @@ void createList();
 void displayList();
 void createListWithTail();  //maintaining a tail pointer
 
+void insertBeg();
+void insertEnd();
+void insertatPOS();
+
+int length();
+
 struct node{
     int data;
     struct node *next;
@@ -15,10 +21,13 @@ struct node *head=NULL, *tail=NULL;
 void main(){
 
     // createList();
-
     createListWithTail();   //linked list with maintained Tail pointer
-    displayList();    
 
+    insertBeg();
+    insertEnd();
+    displayList();
+    insertatPOS();
+    displayList();
 }
 
 void createList(){
@@ -82,3 +91,99 @@ void createListWithTail(){
         scanf("%d",&choice);
     }
 }
+
+void insertBeg(){
+    struct node *new;
+
+    new=(struct node*)malloc(sizeof(struct node));
+    printf("Insert BEGG\nEnter data: (new node): ");
+    scanf("%d",&new->data);
+
+    new->prev=new->next=NULL;
+
+    if(head==NULL){
+        head=new;
+    }else{
+        new->next=head;
+        head->prev=new;
+
+        head=new;
+    }
+}
+
+void insertEnd(){
+    struct node *new;
+
+    new=(struct node*)malloc(sizeof(struct node));
+    printf("Insert END\nEnter data: (new node): ");
+    scanf("%d",&new->data);
+
+    new->prev=new->next=NULL;
+
+    if(head==NULL){
+        insertBeg();
+    }else{
+        tail->next=new;
+        new->prev=tail;
+        tail=new;
+    }
+}
+
+int length(){
+    struct node *temp=head;
+    int count=0;
+
+    while(temp!=NULL){
+        temp=temp->next;
+        count++;
+    }
+
+    return count;
+}
+void insertatPOS(){
+    struct node *new;
+
+    int pos;
+    printf("Enter position: ");
+    scanf("%d",&pos);
+
+    if(pos<1 || pos>length()){        
+        printf("Invalid Position!");
+    }else{
+
+        printf("Insert at %d\n", pos);
+        new=(struct node*)malloc(sizeof(struct node));
+        printf("Enter data: ");
+        scanf("%d",&new->data);
+        
+        if(head==NULL&&pos==1){
+            
+            insertBeg();
+
+        }else if(head!=NULL&&pos==1){
+
+            new->next=head;
+            head->prev=new;
+            head=new;
+
+        }else{
+            
+            struct node *temp;
+            temp=head;
+
+            int i=1;
+            while(i<pos-1){
+                temp=temp->next;
+                i++;
+            }
+
+            new->prev=temp;
+            new->next=temp->next;
+            temp->next=new;
+            new->next->prev=new;
+        }
+
+    }
+}
+
+
